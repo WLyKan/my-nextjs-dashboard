7e5432dc15fdb28dd7d9dcae7753b00a00eb0b02
wangyikan <wangyikan@supcon.com>
2025-12-09 19:48:03 +0800

    feat: eslint 配置

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config'
+
+// 导出默认配置
+export default antfu({
+  // 启用严格规则
+  strict: true,
+  // 启用TypeScript规则
+  typescript: true,
+  // 启用React规则
+  react: true,
+  // includes: [
+  //   'src/**/*.{js,jsx,ts,tsx}',
+  // ],
+  // 配置忽略的文件
+  ignores: [
+    '**/node_modules',
+    '**/dist',
+    '**/.next',
+    '**/build',
+  ],
+})
